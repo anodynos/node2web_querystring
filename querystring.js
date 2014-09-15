ea6f155e4adf353b14b24c7c9d793d7577a915ba
anodynos <agelos.pikoulas@gmail.com>
2014-09-16 02:23:07 +0300

    browserify version '3.24.10'

--- querystring.js
+++ querystring.js
@@ -3,7 +3,7 @@
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'querystring' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10', build date 'Tue Sep 16 2014 00:07:11 GMT+0300 (EEST)' 
+browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:07 GMT+0300 (EEST)' 
 **/
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.querystring=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'querystring' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10', build date 'Tue Sep 16 2014 00:07:11 GMT+0300 (EEST)'
+browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:07 GMT+0300 (EEST)'
